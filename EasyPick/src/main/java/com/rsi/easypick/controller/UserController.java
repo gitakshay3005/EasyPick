a45c1d8e73b9bd8a8aab7c79fda9b0cb1b030f93
akshay sawant <akshay.sawant@retailsolutions.com>
2018-12-11 20:23:00 +0530

    user register and login

---
+++ EasyPick/src/main/java/com/rsi/easypick/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.rsi.easypick.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rsi.easypick.products.beans.ProductSearchResponse;
+import com.rsi.easypick.user.beans.UserBean;
+import com.rsi.easypick.user.beans.UserResponse;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/easypick/api")
+public class UserController {
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST, produces = "application/json")
+	public HttpEntity<UserResponse> registerUser(@RequestBody(required = true)UserBean bean)
+	{
+		UserResponse userResponse = new UserResponse(bean.getId(), bean.getFirstName(), bean.getLastName(), bean.getEmailId(), bean.getUserType());
+		return new ResponseEntity<UserResponse>(userResponse, HttpStatus.OK);
+		
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json")
+	public HttpEntity<UserResponse> login()
+	{
+		
+		UserResponse userResponse = new UserResponse("6", "akshay", "sawant", "akshay.sawant@retailsolutions.com", "c");
+		return new ResponseEntity<UserResponse>(userResponse, HttpStatus.OK);
+	}
+	
+
+}
---
+++ EasyPick/src/main/java/com/rsi/easypick/user/beans/UserLoginRequest.java
@@ -0,0 +1,39 @@
+package com.rsi.easypick.user.beans;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
+public class UserLoginRequest {
+
+	String emailId;
+	String password;
+	
+	@JsonCreator
+	public UserLoginRequest(@JsonProperty("emailId")String emailId,@JsonProperty("password") String password) {
+		super();
+		this.emailId = emailId;
+		this.password = password;
+	}
+	
+	
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
